f2c9674a0b5862e1dbeb3a909a0866be7a9c5c37
djumanov <djumanovdev@gmail.com>
2026-01-08 08:49:42 +0500

    home page

---
+++ blog/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
+
+
+def home_page(request: HttpRequest) -> HttpResponse:
+
+    return render(request=request, template_name='home.html')
+    
